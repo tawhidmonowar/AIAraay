95f52a4200d13873d59bb090f56929eec56603a6
tawhidmonowar <tawhidmonowar@gmail.com>
2021-09-08 20:51:43 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import nltk
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+with open("intents.json") as file:
+    data = json.load(file)
+print(data)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    
