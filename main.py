172365729427c17d0dcb6933b591524113667171
tawhidmonowar <tawhidmonowar@gmail.com>
2021-09-09 13:19:35 +0600

    09-09-2021

--- main.py
+++ main.py
@@ -9,5 +9,18 @@ stemmer = LancasterStemmer()
 
 with open("intents.json") as file:
     data = json.load(file)
-print(data)
-    
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
